df353c716dde529e875a83a6ff9e2604f529cde1
Makka <makkanev@gmail.com>
2025-02-10 16:42:50 +0300

    use plagin sonar

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -3,24 +3,26 @@ package app.model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class AnimalsCage {
+    private static final Logger logger = LoggerFactory.getLogger(AnimalsCage.class);
 
-    @Qualifier("dog")
-    @Autowired
-    private Animal animal;
+    private final Animal animal;
+    private final Timer timer;
 
-    @Qualifier("timer")
     @Autowired
-    private Timer timer;
+    public AnimalsCage(@Qualifier("dog") Animal animal, @Qualifier("timer") Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
-        System.out.println("Say:");
-        System.out.println(animal.toString());
-        System.out.println("At:");
-        System.out.println(timer.getTime());
-        System.out.println("________________________");
+        logger.info("Say: {}", animal);
+        logger.info("At: {}", timer.getTime());
+        logger.info("________________________");
     }
 
     public Timer getTimer() {
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,9 +8,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 public class MainTest {
+
+
 
     @Autowired
     private ApplicationContext applicationContext;
